feef888de30b106529b2a73744628a86def42b66
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2022-09-23 17:25:30 -0300

    wip

--- src/renderer/views/HomeView.vue
+++ src/renderer/views/HomeView.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
+import { ref } from "vue"
+
+let message = ref("Hello World!")
+
+const showMessage = () => alert(message.value)
+
 window.electron.ipcRenderer.send("message", "Hello from Home View")
 </script>
 
-<template>Test</template>
+<template>
+    {{ message }}
+
+    <p>
+        <input type="text" v-model="message" />
+    </p>
+
+    <button @click="showMessage()">Click me</button>
+</template>
