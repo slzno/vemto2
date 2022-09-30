b008df87ef406c4b8e0a62d48c7ae82da753cf89
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2022-09-30 13:28:30 -0300

    Trying to compile PHP as PHAR

---
+++ src/php/apps/schema-reader/load.php
@@ -0,0 +1,12 @@
+#!/usr/bin/env php
+<?php
+
+$APP_DIRECTORY = getcwd();
+
+echo $APP_DIRECTORY . "\n";
+
+define('LARAVEL_START', microtime(true));
+
+require $APP_DIRECTORY . '/vendor/autoload.php';
+
+$app = require_once $APP_DIRECTORY . '/bootstrap/app.php';
