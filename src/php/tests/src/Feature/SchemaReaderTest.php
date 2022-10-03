3d8e67b3bc7ca5473ec94ee7295402c4f5916133
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2022-10-03 16:52:37 -0300

    Adding common PHP libraries and starting php tests

---
+++ src/php/tests/src/Feature/SchemaReaderTest.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class SchemaReaderTest extends TestCase {
+
+    public function test_it_correctly_reads_laravel_migrations()
+    {
+        $this->assertTrue(true);
+    }
+
+}
