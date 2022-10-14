9be11b8bb989d2b51efec794395a1d932deac28f
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2022-10-14 18:28:22 -0300

    wip

--- src/main/DatabaseHandler.ts
+++ src/main/DatabaseHandler.ts
@@ -1,5 +1,6 @@
 import { ipcMain } from "electron"
 import RelaDB from "@tiago_silva_pereira/reladb"
+import Project from "../common/models/Project"
 
 export function HandleDatabase() {
     const database = new RelaDB.Database
@@ -7,17 +8,10 @@ export function HandleDatabase() {
 
     RelaDB.Resolver.setDatabase(database)
     RelaDB.Resolver.db().driver.feedDatabaseData({})
-    
-    RelaDB.Resolver.db().onDataChanged(() => {
-        const updatedData = RelaDB.Resolver.db().driver.getDatabaseData()
-        console.log(updatedData)
-    })
-
-    console.log('Starting database')
 
     ipcMain.handle("database:data:updated", (event, data) => {
         RelaDB.Resolver.db().driver.feedDatabaseData(data)
 
-        console.log('Database data changed', data)
+        console.log('project', Project.find(1))
     })
 }
