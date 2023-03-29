4a2eb10f52c75e0c3226edb5a1af56793d7e06c1
nicollassilva <lyod.hp@gmail.com>
2023-03-29 16:43:49 -0300

    Adding UiModal to create new tables

--- src/renderer/views/components/ProjectSchema/SchemaHeader.vue
+++ src/renderer/views/components/ProjectSchema/SchemaHeader.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts">
+    import { ref } from 'vue'
+    import Table from "@Common/models/Table"
     import { ArrowPathIcon } from "@heroicons/vue/24/outline"
+    import UiModal from '@Renderer/components/ui/UiModal.vue'
+
+    const showingCreateTableModal = ref(false)
+
+    const createTable = (): void => {
+        
+    }
+
+    const show = () => {
+        showingCreateTableModal.value = true
+    }
+
+    const close = () => {
+        showingCreateTableModal.value = false
+    }
 </script>
 
 <template>
@@ -11,6 +28,7 @@
             <div class="flex">
                 <div
                     class="p-2 cursor-pointer text-slate-400 hover:text-red-500"
+                    @click="createTable()"
                 >
                     <svg
                         class="w-7 h-7"
@@ -27,6 +45,14 @@
                         ></path>
                     </svg>
                 </div>
+
+                <UiModal
+                    title="Create Table"
+                    :show="showingCreateTableModal"
+                    @close="close()"
+                >
+                    Teste
+                </UiModal>
 
                 <!-- <div class="p-2 cursor-pointer text-slate-600 hover:text-red-500">
                         <svg class="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
