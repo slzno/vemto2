be9f3bbb7e0ba4728f53828a4d452c33ee822e7f
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2023-04-12 16:47:16 -0300

    wip

--- src/renderer/views/ProjectPageView.vue
+++ src/renderer/views/ProjectPageView.vue
@@ -4,6 +4,7 @@
     import { onMounted, ref } from 'vue'
     import { useRoute } from 'vue-router'
     import Page from '@Common/models/Page'
+import UiButton from '@Renderer/components/ui/UiButton.vue';
 
     // const projectStore = useProjectStore()
 
@@ -22,7 +23,16 @@
         class="bg-slate-100 dark:bg-slate-900 w-full h-full relative overflow-hidden p-4"
     >
         <div v-if="page">
-            Page editor {{ pageId }} {{ page.getFileName() }}
+            Page editor: {{ page.getFileName() }}
+
+            <div>
+                <UiButton>Add Section</UiButton>
+                <UiButton>Add Datatable</UiButton>
+                <UiButton>Add Text Input</UiButton>
+                <UiButton>Add Button</UiButton>
+                <UiButton>Add Hero</UiButton>
+                <UiButton>Add Title</UiButton>
+            </div>
         </div>
     </div>
 </template>
