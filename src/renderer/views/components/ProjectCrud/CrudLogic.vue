33728b9e0dba4c34343f7b256891a5adfd3a4ae9
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2023-06-16 17:15:54 -0300

    Adding monaco editor to edit logic

--- src/renderer/views/components/ProjectCrud/CrudLogic.vue
+++ src/renderer/views/components/ProjectCrud/CrudLogic.vue
@@ -6,25 +6,13 @@
     import RenderableLivewireCreateComponent from "@Renderer/codegen/sequential/services/crud/views/livewire/RenderableLivewireCreateComponent"
     import RenderableLivewireEditComponent from "@Renderer/codegen/sequential/services/crud/views/livewire/RenderableLivewireEditComponent"
 
-    // CodeMirror
-    import { indentWithTab } from "@codemirror/commands"
-    import { EditorState, Transaction } from "@codemirror/state"
-    import { EditorView, basicSetup } from "codemirror"
-    import {
-        keymap,
-        Decoration,
-        ViewPlugin,
-        ViewUpdate,
-        DecorationSet,
-        MatchDecorator,
-    } from "@codemirror/view"
-    import { php as phpLang } from "@codemirror/lang-php"
-    import readOnlyRangesExtension, {
-        preventModifyTargetRanges,
-        smartPaste,
-        smartDelete,
-    } from "codemirror-readonly-ranges"
-    import { oneDark } from "@codemirror/theme-one-dark"
+    import * as monaco from 'monaco-editor'
+    import { constrainedEditor } from "constrained-editor-plugin"
+    import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+    import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+    import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+    import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+    import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
     const props = defineProps({
         crud: {
@@ -40,8 +28,7 @@
 
     let initialHookLines = [],
         createComponentContent = "",
-        startState = null,
-        view = null
+        editor = null
 
     const tabs = [
         { label: "Create Component", value: "createComponent" },
@@ -52,182 +39,39 @@
         createComponentContent = await new RenderableLivewireCreateComponent(
             crud.value
         ).compile()
-        editComponentContent.value = await new RenderableLivewireEditComponent(
-            crud.value
-        ).compile()
 
         initialHookLines = getHookLines(createComponentContent)
 
-        startState = createEditorState(
-            replaceHookLinesWithNoContent(createComponentContent)
-        )
-
-        view = new EditorView({
-            state: startState,
-            parent: createComponentEditor.value,
-        })
-    })
-
-    const createEditorState = (content: string) => {
-        const simpleExtension = () => {
-            let currentLine = null,
-                previousLine = null
-
-            return EditorState.changeFilter.of((tr: Transaction) => {
-                previousLine = currentLine
-                // console.log(tr.startState.doc.toString())
-
-                // console log current line
-                currentLine = tr.state.doc.lineAt(
-                    tr.state.selection.main.head
-                ).number
-
-                // console.log(previousLine, currentLine)
-
-                const currentLineContent = tr.state.doc.lineAt(
-                    tr.state.selection.main.head
-                ).text
-
-                console.log(previousLine, currentLine, currentLineContent)
-
-                if (currentLineContent.includes("// hook:")) {
-                    // console.log("hook line")
-
-                    // change line opacity to 75%
-
-                    return true
-                }
-
-                return false
-            })
-        }
-
-        const simpleTransactionFilterExtension = () => {
-            return EditorState.transactionFilter.of((tr: Transaction) => {
-                console.log(
-                    tr.state.doc.lineAt(tr.state.selection.main.head).text
-                )
-                return tr
-            })
-        }
-
-        const simpleEventHandlers = () => {
-            return EditorView.domEventHandlers({
-                keyup: (event, view) => {
-                    const currentLine = view.state.doc.lineAt(
-                        view.state.selection.main.head
-                    ).number
-                    // log current line
-                    // console.log(view.state.doc.lineAt(view.state.selection.main.head).text)
-
-                    // console.log(event)
-
-                    // if is tab, stop propagation
-                    if (event.key === "Enter") {
-                        console.log(currentLine)
-                        const currentLineIsHookLine =
-                            initialHookLines.includes(currentLine)
-
-                        // duplicate the line on createComponentContent if it is a hook line
-                        if (currentLineIsHookLine) {
-                            const contentLines =
-                                    createComponentContent.split("\n"),
-                                currentLineContent =
-                                    contentLines[currentLine - 1],
-                                currentLineIndentation =
-                                    currentLineContent.match(/^\s+/)
-
-                            let newLineContent = ""
-
-                            // console.log(currentLineContent, currentLineIndentation)
-
-                            // add a new line
-                            newLineContent += "\n"
-
-                            // add indentation
-                            // newLineContent += currentLineIndentation ? currentLineIndentation[0] : ""
-
-                            // add the hook comment
-                            newLineContent += currentLineContent
-
-                            // insert new line content after the current line on createComponentContent
-                            createComponentContent =
-                                createComponentContent.replace(
-                                    currentLineContent,
-                                    currentLineContent + newLineContent
-                                )
-
-                            // update the hook lines (inser after the current line)
-                            initialHookLines = getHookLines(
-                                createComponentContent
-                            )
-
-                            // add a new line to the editor
-                            view.dispatch({
-                                changes: {
-                                    from: view.state.selection.main.head,
-                                    insert: "\n",
-                                },
-                            })
-                        }
-                    }
-                },
-            })
-        }
-
-        // simple extension that adds opacity to the lines that are not hook lines
-        const addOpacityToNonHookLines = () => {
-            const allLines = getLineNumbers(content),
-                nonHookLines = getNonHookLines(createComponentContent),
-                hookLines = getHookLines(createComponentContent)
-
-            const createDecorations = (view) => {
-                let decorations = []
-
-                const isHookLine = (lineNumber) => {
-                    return hookLines.includes(lineNumber)
-                }
-
-                for (let lineNumber of allLines) {
-                    let line = view.state.doc.line(lineNumber);
-                    
-                    if (!isHookLine(lineNumber)) {
-                        decorations.push(Decoration.line({class: "opacity-75 cursor-not-allowed readonly"}).range(line.from, line.from));
-                    } else {
-                        decorations.push(Decoration.line({class: "bg-slate-900"}).range(line.from, line.from));
-                    }
-                }
-
-                return Decoration.set(decorations)
+        self.MonacoEnvironment = {
+            createTrustedTypesPolicy(policyName, policyOptions) { return undefined },
+            getWorker(_, label) {
+                if (label === 'json') return new jsonWorker()
+                if (label === 'css' || label === 'scss' || label === 'less') return new cssWorker()
+                if (label === 'html' || label === 'handlebars' || label === 'razor') return new htmlWorker()
+                if (label === 'typescript' || label === 'javascript') return new tsWorker()
+                
+                return new editorWorker()
             }
-
-            return ViewPlugin.define(
-                (view) => ({
-                    decorations: createDecorations(view),
-                }),
-                {
-                    decorations: (view) => view.decorations,
-                }
-            )
         }
 
-        return EditorState.create({
-            doc: content,
-            extensions: [
-                oneDark,
-                basicSetup,
-                keymap.of([indentWithTab]),
-                simpleEventHandlers(),
-                // simpleExtension(),
-                // simpleTransactionFilterExtension(),
-                // readOnlyRangesExtension(getReadOnlyRanges),
-                addOpacityToNonHookLines(),
-                // preventModifyTargetRanges(getReadOnlyRanges),
-                // smartDelete(getReadOnlyRanges),
-                phpLang(),
-            ],
+        editor = monaco.editor.create(document.getElementById('createComponentEditor'), {
+            value: "function hello() {\ncontent\n}",
+            language: 'javascript',
+            minimap: {
+                enabled: false
+            },
         })
-    }
+
+        const model = editor.getModel()
+
+        const constrainedInstance = constrainedEditor(monaco);
+        constrainedInstance.initializeIn(editor);
+        constrainedInstance.addRestrictionsTo(model, [{
+            range: [2, 1, 2, 8], // Range of Util Variable name
+            label: 'functionBody',
+            allowMultiline: true,
+        }]);
+    })
 
     const replaceHookLinesWithNoContent = (content: string) => {
         let lines = content.split("\n")
@@ -282,49 +126,47 @@
         return lineNumbers
     }
 
-    const getReadOnlyRanges = (
-        targetState: EditorState
-    ): Array<{ from: number | undefined; to: number | undefined }> => {
-        const hookLines = initialHookLines,
-            lastestHookLine = hookLines[hookLines.length - 1],
-            lastestLine = targetState.doc.lines
+    // const getReadOnlyRanges = (): Array<{ from: number | undefined; to: number | undefined }> => {
+    //     const hookLines = initialHookLines,
+    //         lastestHookLine = hookLines[hookLines.length - 1],
+    //         lastestLine = targetState.doc.lines
 
-        let ranges = []
+    //     let ranges = []
 
-        hookLines.forEach((line, index) => {
-            const previousHookLine = hookLines[index - 1] ?? undefined,
-                previousLine = line === 1 ? undefined : line - 1
+    //     hookLines.forEach((line, index) => {
+    //         const previousHookLine = hookLines[index - 1] ?? undefined,
+    //             previousLine = line === 1 ? undefined : line - 1
 
-            if (index === 0) {
-                ranges.push({
-                    from: 0,
-                    to: previousLine
-                        ? targetState.doc.line(previousLine).to
-                        : undefined,
-                })
-            } else {
-                ranges.push({
-                    from: previousHookLine
-                        ? targetState.doc.line(previousHookLine).to
-                        : undefined,
-                    to: previousLine
-                        ? targetState.doc.line(previousLine).to
-                        : undefined,
-                })
-            }
-        })
+    //         if (index === 0) {
+    //             ranges.push({
+    //                 from: 0,
+    //                 to: previousLine
+    //                     ? targetState.doc.line(previousLine).to
+    //                     : undefined,
+    //             })
+    //         } else {
+    //             ranges.push({
+    //                 from: previousHookLine
+    //                     ? targetState.doc.line(previousHookLine).to
+    //                     : undefined,
+    //                 to: previousLine
+    //                     ? targetState.doc.line(previousLine).to
+    //                     : undefined,
+    //             })
+    //         }
+    //     })
 
-        if (lastestHookLine < lastestLine) {
-            ranges.push({
-                from: lastestHookLine
-                    ? targetState.doc.line(lastestHookLine).to
-                    : undefined,
-                to: undefined,
-            })
-        }
+    //     if (lastestHookLine < lastestLine) {
+    //         ranges.push({
+    //             from: lastestHookLine
+    //                 ? targetState.doc.line(lastestHookLine).to
+    //                 : undefined,
+    //             to: undefined,
+    //         })
+    //     }
 
-        return ranges
-    }
+    //     return ranges
+    // }
 </script>
 
 <template>
@@ -339,7 +181,9 @@
                 class="overflow-y-auto font-mono text-lg"
                 style="height: calc(100% - 150px)"
                 ref="createComponentEditor"
+                id="createComponentEditor"
             ></div>
+            
         </div>
 
         <div
