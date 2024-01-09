1696f07a74628291a52fdd4e57c4c9fdb25c6eca
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2024-01-09 16:22:11 -0300

    wip

--- src/renderer/codegen/sequential/services/model/GenerateModelFiles.ts
+++ src/renderer/codegen/sequential/services/model/GenerateModelFiles.ts
@@ -1,5 +1,4 @@
 import Model from "@Common/models/Model"
-import RenderableModel from "./RenderableModel"
 import RenderableFactory from "./RenderableFactory"
 
 export default class GenerateModelFiles {
@@ -7,7 +6,6 @@ export default class GenerateModelFiles {
         const models = Model.getValid()
 
         for (const model of models) {
-            // await new RenderableModel(model).render()
             await new RenderableFactory(model).render()
         }
     }
