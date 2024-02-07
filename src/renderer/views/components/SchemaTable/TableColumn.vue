be424462b4d1f99cc50fac035cd3c8e18cd843ea
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2024-02-07 18:09:55 -0300

    wip

--- src/renderer/views/components/SchemaTable/TableColumn.vue
+++ src/renderer/views/components/SchemaTable/TableColumn.vue
@@ -8,7 +8,7 @@
 <template>
     <div :class="{
         'line-through opacity-50': column.isRemoved()
-    }" class="w-full flex items-center text-slate-700 dark:text-slate-400 dark:hover:bg-slate-800 px-1 rounded">
+    }" class="w-full flex items-center text-slate-700 dark:text-slate-400 dark:hover:bg-slate-800 px-1 rounded cursor-default">
         <span
             :class="{
                 'text-yellow-500 dark:text-yellow-400 font-semibold':
