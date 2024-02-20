f12c92da96424b9064c426251765bce02b01be83
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2024-02-20 11:41:44 -0300

    wip

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -82,7 +82,7 @@
             <!-- Current Project: vemto-test-01 -->
         </div>
     </div>
-    <div id="appContainer" class="w-full h-screen">
+    <div id="appContainer" class="w-full h-screen dark">
         <div class="text-slate-700 dark:text-slate-300 h-full w-full">
             <RouterView :key="$route.fullPath" />
         </div>
