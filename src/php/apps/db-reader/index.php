2ee03b22fabf8c8901cb392408fbeddf359163db
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2024-03-18 12:47:48 -0300

    wip

--- src/php/apps/db-reader/index.php
+++ src/php/apps/db-reader/index.php
@@ -1,6 +1,9 @@
 <?php
 
 // Load the basic Laravel classes
+
+use Illuminate\Support\Facades\DB;
+
 require_once 'load.php';
 
 // Load Vemto classes
@@ -23,7 +26,42 @@ Vemto::execute('schema-reader', function () use ($app, $APP_DIRECTORY) {
         new Symfony\Component\Console\Output\ConsoleOutput
     );
 
-    Vemto::log('Schema has been read successfully!');
+    $previousConnection = DB::getDefaultConnection();
+
+    Vemto::log('Previous connection: ' . $previousConnection);
+
+    // try {
+    //     $this->setup($previousConnection);
+
+    //     $connection = $this->option('connection') ?: $previousConnection;
+
+    //     DB::setDefaultConnection($connection);
+
+    //     $this->schema = $this->makeSchema();
+
+    //     $this->info('Using connection: ' . $connection . "\n");
+
+    //     $tables       = $this->filterTables()->sort()->values();
+    //     $views        = $this->filterViews()->sort()->values();
+    //     $generateList = $tables->merge($views)->unique();
+
+    //     $this->info('Generating migrations for: ' . $generateList->implode(',') . "\n");
+
+    //     $this->askIfLogMigrationTable($previousConnection);
+
+    //     $this->generate($tables, $views);
+
+    //     $this->info("\nFinished!\n");
+
+    //     if (DB::getDriverName() === Driver::SQLITE->value) {
+    //         $this->warn('SQLite only supports foreign keys upon creation of the table and not when tables are altered.');
+    //         $this->warn('See https://www.sqlite.org/omitted.html');
+    //         $this->warn('*_add_foreign_keys_* migrations were generated, however will get omitted if migrate to SQLite type database.');
+    //     }
+    // } finally {
+    //     DB::setDefaultConnection($previousConnection);
+    //     app()->forgetInstance(Setting::class);
+    // }
 
     Vemto::respondWith([
         'status' => 'success'
