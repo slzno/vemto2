c234ea4062812757510837a6412cdda4ebc41a45
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2024-03-19 18:18:24 -0300

    Trying to boot the application with the new Laravel 11 booter

--- src/php/apps/db-reader/index.php
+++ src/php/apps/db-reader/index.php
@@ -10,22 +10,53 @@ require_once 'classes/ExtendedKernel.php';
 require_once 'classes/ReadTablesFromDatabase.php';
 require_once 'classes/ModelRepository.php';
 
+use Illuminate\Foundation\Application;
+
+class DumbExceptionHandler implements Illuminate\Contracts\Debug\ExceptionHandler
+{
+    public function report(Throwable $e)
+    {
+        //
+    }
+
+    public function render($request, Throwable $e)
+    {
+        throw $e;
+    }
+
+    public function renderForConsole($output, Throwable $e)
+    {
+        throw $e;
+    }
+
+    public function shouldReport(Throwable $e)
+    {
+        return false;
+    }
+}
+
 Vemto::execute('schema-reader', function () use ($app, $APP_DIRECTORY) {
     // Set the database connection to SQLite
     // config(['database.default' => 'sqlite']);
     // config(['database.connections.sqlite.database' => ':memory:']);
 
     // Start the application with the extended kernel
-    $app->bind(Illuminate\Contracts\Console\Kernel::class, ExtendedKernel::class);
-    $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+    // $app->bind(Illuminate\Contracts\Console\Kernel::class, ExtendedKernel::class);
+    // $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 
-    $kernel->handle(
-        new Symfony\Component\Console\Input\ArgvInput,
-        new Symfony\Component\Console\Output\ConsoleOutput
-    );
+    // $kernel->handle(
+    //     new Symfony\Component\Console\Input\ArgvInput,
+    //     new Symfony\Component\Console\Output\ConsoleOutput
+    // );
+    
+    $app = Application::configure(basePath: dirname(__DIR__))
+        ->withExceptions([DumbExceptionHandler::class])
+        ->create();
 
-    // $reader = new ReadTablesFromDatabase();
-    // $reader->handle();
+    $app->handleCommand(new Symfony\Component\Console\Input\ArgvInput);
+
+    $reader = new ReadTablesFromDatabase();
+    $reader->handle();
 
     Vemto::respondWith([
         'status' => 'success'
