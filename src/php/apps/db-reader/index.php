63a3e39954998ab03ca6db8180fbe9b5ac4d8854
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2024-03-20 12:44:52 -0300

    Booting the laravel application

--- src/php/apps/db-reader/index.php
+++ src/php/apps/db-reader/index.php
@@ -11,55 +11,67 @@ require_once 'classes/ReadTablesFromDatabase.php';
 require_once 'classes/ModelRepository.php';
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Facade;
 
-class DumbExceptionHandler implements Illuminate\Contracts\Debug\ExceptionHandler
-{
-    public function report(Throwable $e)
-    {
-        //
-    }
-
-    public function render($request, Throwable $e)
-    {
-        throw $e;
-    }
-
-    public function renderForConsole($output, Throwable $e)
-    {
-        throw $e;
-    }
-
-    public function shouldReport(Throwable $e)
-    {
-        return false;
-    }
-}
+// use Illuminate\Foundation\Configuration\Exceptions;
 
 Vemto::execute('schema-reader', function () use ($app, $APP_DIRECTORY) {
-    // Set the database connection to SQLite
-    // config(['database.default' => 'sqlite']);
-    // config(['database.connections.sqlite.database' => ':memory:']);
+    $tempPath = sys_get_temp_dir();
+    $_ENV['LARAVEL_STORAGE_PATH'] = $tempPath;
+    // $_ENV['APP_SERVICES_CACHE'] = $tempPath . '/services.php';
 
-    // Start the application with the extended kernel
-    // $app->bind(Illuminate\Contracts\Console\Kernel::class, ExtendedKernel::class);
-    // $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-
-    // $kernel->handle(
-    //     new Symfony\Component\Console\Input\ArgvInput,
-    //     new Symfony\Component\Console\Output\ConsoleOutput
-    // );
-    
     $app = Application::configure(basePath: dirname(__DIR__))
         ->withExceptions(function () {})
         ->create();
 
+    $app->useBootstrapPath(sys_get_temp_dir());
+    $app->useStoragePath(sys_get_temp_dir()); // Use the system temp directory for storage operations
+
+    // dd($app->bootstrapPath());
+
+    $tempManifestPath = sys_get_temp_dir();
+
+    $app->bind('Illuminate\Foundation\PackageManifest', function ($app) use ($tempManifestPath) {
+        return new \Illuminate\Foundation\PackageManifest(
+            new \Illuminate\Filesystem\Filesystem,
+            $app['path.base'],
+            $tempManifestPath . '/packages.php'
+        );
+    });
+
+    $app->bootstrapWith([
+        \Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::class,
+        \Illuminate\Foundation\Bootstrap\LoadConfiguration::class,
+        // \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
+        \Illuminate\Foundation\Bootstrap\RegisterFacades::class,
+        \Illuminate\Foundation\Bootstrap\RegisterProviders::class,
+        \Illuminate\Foundation\Bootstrap\BootProviders::class,
+    ]);
+
     $app->handleCommand(new Symfony\Component\Console\Input\ArgvInput);
 
-    $reader = new ReadTablesFromDatabase();
-    $reader->handle();
+    Facade::setFacadeApplication($app);
+
+    Config::set('database.connections.memory_sqlite', [
+        'driver'   => 'sqlite',
+        'database' => ':memory:',
+    ]);
+
+    DB::setDefaultConnection('memory_sqlite');
+
+    // migrate
+    $migrator = $app->make('migrator');
+    $migrator->run($APP_DIRECTORY . '/database/migrations');
+
+    $result = DB::table("password_resets")->get();
+
+    // $reader = new ReadTablesFromDatabase();
+    // $reader->handle();
 
     Vemto::respondWith([
-        'status' => 'success'
+        'status' => 'success',
+        'result' => $result
     ]);
 });
--- src/php/apps/db-reader/load.php
+++ src/php/apps/db-reader/load.php
@@ -6,5 +6,3 @@ $SCRIPT_DIRECTORY = __DIR__;
 define('LARAVEL_START', microtime(true));
 
 require $SCRIPT_DIRECTORY . '/vendor/autoload.php';
-
-$app = require_once $APP_DIRECTORY . '/bootstrap/app.php';
