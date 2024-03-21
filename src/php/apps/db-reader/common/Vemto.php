d0791845c5d670675d8c0c5a8b65fdc511a2e093
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2024-03-21 18:35:02 -0300

    wip

---
+++ src/php/apps/db-reader/common/Vemto.php
@@ -0,0 +1,179 @@
+<?php // ATTENTION: Don't change this file. Instead, change the original file under src/php/common
+
+namespace Vemto;
+
+use Exception;
+
+class Vemto
+{
+
+    const SUCCESS = 0;
+    const FAILURE = 1;
+
+    public static function respondWith($data)
+    {
+        echo self::jsonResponse($data);
+
+        exit(self::SUCCESS);
+    }
+
+    public static function dump(mixed $data)
+    {
+        $text = print_r($data, true);
+
+        self::log($text);
+    }
+
+    public static function log($message, $type = 'info')
+    {
+        $typeHeaders = [
+            "info" => "[INFO]",
+            "error" => "[ERROR]",
+            "warning" => "[WARNING]",
+            "success" => "[SUCCESS]",
+        ];
+
+        if (getenv('VEMTO_DEBUG')) {
+            $logFilePath = realpath(__DIR__ . '/../../../out/');
+            $logFile = $logFilePath . '/apps.log';
+        } else {
+            $logFile = getcwd() . '/vemto.log';
+        }
+
+        // If the log file doesn't exist, create it
+        // If we are not in debug mode, we also clear the log file
+        if (!file_exists($logFile) || !getenv('VEMTO_DEBUG')) {
+            file_put_contents($logFile, '');
+        }
+
+        file_put_contents($logFile, $typeHeaders[$type] . PHP_EOL . $message . PHP_EOL . PHP_EOL, FILE_APPEND);
+    }
+
+    public static function clearLog()
+    {
+        $logFilePath = realpath(__DIR__ . '/../../../out/');
+        $logFile = $logFilePath . '/apps.log';
+
+        file_put_contents($logFile, '');
+    }
+
+    public static function jsonResponse($data)
+    {
+        $jsonResponse = json_encode($data);
+
+        return "VEMTO_JSON_RESPONSE_START(" . $jsonResponse . ")VEMTO_JSON_RESPONSE_END";
+    }
+
+    public static function writeProcessedFile($fileContent)
+    {
+        $processedFilesFolder = getcwd() . '/.vemto/processed-files';
+        $processedFileName = Illuminate\Support\Str::random(32) . '.php';
+        $processedFilePath = $processedFilesFolder . '/' . $processedFileName;
+
+        if (!file_exists($processedFilesFolder)) {
+            mkdir($processedFilesFolder);
+        }
+
+        file_put_contents($processedFilePath, $fileContent);
+
+        return [
+            'relativePath' => str_replace(getcwd(), '', $processedFilePath),
+            'path' => $processedFilePath,
+            'name' => $processedFileName,
+        ];
+    }
+
+    public static function writeConflictsFile($conflicts)
+    {
+        $conflictsFileFolder = getcwd() . '/.vemto/conflicts';
+        $conflictsFileName = Illuminate\Support\Str::random(32) . '.json';
+        $conflictsFilePath = $conflictsFileFolder . '/' . $conflictsFileName;
+
+        if (!file_exists($conflictsFileFolder)) {
+            mkdir($conflictsFileFolder);
+        }
+
+        $conflictsFileContent = json_encode($conflicts, JSON_PRETTY_PRINT);
+
+        file_put_contents($conflictsFilePath, $conflictsFileContent);
+
+        return [
+            'relativePath' => str_replace(getcwd(), '', $conflictsFilePath),
+            'path' => $conflictsFilePath,
+            'name' => $conflictsFileName,
+        ];
+    }
+
+    public static function execute(string $appName, $callback = null)
+    {
+        try {
+            if ($callback) {
+                $callback();
+            }
+
+            exit(static::SUCCESS);
+        } catch (\Throwable $th) {
+            if (getenv('VEMTO_DEBUG')) {
+                Vemto::log($th->getMessage(), 'error');
+                Vemto::log($th->getTraceAsString(), 'error');
+            }
+
+            $traceString = str_replace("\n", "VEMTO_EOL", $th->getTraceAsString());
+
+            echo "VEMTO_ERROR_START({$appName} Error: " . $th->getMessage() . ")VEMTO_ERROR_END";
+            echo "VEMTO_ERROR_TRACE_START({$appName} Error: " . $traceString . ")VEMTO_ERROR_TRACE_END";
+
+            exit(static::FAILURE);
+        }
+    }
+
+    public static function runPhpCsFixer($fileContent)
+    {
+        $staticFolder = Vemto::getStaticFolder();
+        $phpCsFixerPath = $staticFolder . '/tools/php-cs-fixer/vendor/bin/php-cs-fixer';
+        $phpCsFixerSettingsPath = $staticFolder . '/tools/php-cs-fixer/.php-cs-fixer.php';
+
+        if (!file_exists($phpCsFixerPath)) {
+            throw new Exception("PHP CS Fixer not found on internal build");
+        }
+
+        // run cs fixer on the generated code
+        $newFileContent = Vemto::manipulateTemporaryFile($fileContent, function ($temporaryFilePath) use ($phpCsFixerPath, $phpCsFixerSettingsPath) {
+            $command = "{$phpCsFixerPath} fix {$temporaryFilePath} --using-cache=no --config={$phpCsFixerSettingsPath}";
+
+            shell_exec($command);
+
+            return file_get_contents($temporaryFilePath);
+        });
+
+        return $newFileContent;
+    }
+
+    public static function manipulateTemporaryFile($fileContent, $manipulationCallback = null)
+    {
+        $newFileContent = $fileContent;
+
+        $temporaryFilePath = tempnam(sys_get_temp_dir(), 'vemto');
+
+        file_put_contents($temporaryFilePath, $fileContent);
+
+        if ($manipulationCallback) {
+            $newFileContent = $manipulationCallback($temporaryFilePath);
+        }
+
+        unlink($temporaryFilePath);
+
+        return $newFileContent;
+    }
+
+    public static function getStaticFolder()
+    {
+        // If we are running from a phar file, the static folder is in the same directory
+        if (strpos(__FILE__, 'phar://') === 0) {
+            $pharLocation = str_replace('\\', '/', $_SERVER['SCRIPT_FILENAME']);
+            return dirname($pharLocation);
+        }
+
+        return realpath(__DIR__ . '/../../../../main/static');
+    }
+}
--- src/renderer/codegen/sequential/SequentialGenerator.ts
+++ src/renderer/codegen/sequential/SequentialGenerator.ts
@@ -22,6 +22,15 @@ export default class SequentialGenerator {
         this.project = project
     }
 
+    // async checkDependencies(): Promise<boolean> {
+        // Renderable.setMode("checker")
+        // await this.runGenerators()
+        // Renderable.setMode("generate")
+        // return await PackageChecker.hasMissingDependencies()
+
+        // JetstreamInstaller.run()
+    // }
+
     async run() {
         try {
             await this.runGeneration() 
@@ -43,6 +52,20 @@ export default class SequentialGenerator {
         this.project.clearCurrentRenderedFilesPaths()
         this.project.clearRemovedRenderableFiles()
 
+        await this.runGenerators()
+
+        this.project.processRemovableFiles()
+
+        await this.waitForProcessingFilesQueue()
+
+        await this.readSchema()
+
+        SchemaBuilder.enableSchemaChangesCheck()
+
+        SequentialGenerator.stopTimer()
+    }
+
+    async runGenerators() {
         await new GenerateUiComponentsFiles().start()
 
         await new GenerateMenu().start()
@@ -58,16 +81,6 @@ export default class SequentialGenerator {
         
         await new GenerateDatabaseSeeder().start()
         await new GenerateLivewireLayout().start()
-
-        this.project.processRemovableFiles()
-
-        await this.waitForProcessingFilesQueue()
-
-        await this.readSchema()
-
-        SchemaBuilder.enableSchemaChangesCheck()
-
-        SequentialGenerator.stopTimer()
     }
 
     async clearVemtoFolders() {
