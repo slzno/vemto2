5e1ea5043ec99f19ea2d3980ab33bf8e541d4d4a
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2024-04-16 16:44:20 -0300

    wip

--- src/renderer/components/ui/UiDropdownItem.vue
+++ src/renderer/components/ui/UiDropdownItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <li class="flex items-center justify-start text-md p-1 cursor-pointer hover:bg-slate-100 dark:hover:bg-slate-850 px-3 rounded">
+    <li class="flex items-center justify-start text-md p-1 cursor-pointer hover:bg-slate-100 dark:hover:bg-slate-850 px-3 rounded text-slate-700 dark:text-slate-300">
         <slot></slot>
     </li>
 </template>
