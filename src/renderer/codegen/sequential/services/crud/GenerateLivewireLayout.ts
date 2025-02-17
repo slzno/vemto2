36295132544dc56b880f133c728c76a09a82a9e9
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2025-02-17 12:51:49 -0300

    Renaming RenderableLivewireLayout to LivewireLayoutRenderable

--- src/renderer/codegen/sequential/services/crud/GenerateLivewireLayout.ts
+++ src/renderer/codegen/sequential/services/crud/GenerateLivewireLayout.ts
@@ -1,10 +1,10 @@
 import Project from "@Common/models/Project";
-import RenderableLivewireLayout from "./livewire/RenderableLivewireLayout";
+import LivewireLayoutRenderable from "./livewire/LivewireLayoutRenderable";
 
 export default class GenerateLivewireLayout {
     async start(project: Project) {
         if(!project.codeGenerationSettings.livewireLayout) return;
         
-        await new RenderableLivewireLayout().render()
+        await new LivewireLayoutRenderable().render()
     }
 }
