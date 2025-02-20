3b7f150f912562d276f261ed3deb77c55939641d
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2025-02-20 17:11:44 -0300

    Adding disabled to the UI dropdown item

--- src/renderer/components/ui/UiDropdownItem.vue
+++ src/renderer/components/ui/UiDropdownItem.vue
@@ -1,5 +1,32 @@
 <template>
-    <li class="flex items-center justify-start text-md p-1 cursor-pointer hover:bg-slate-100 dark:hover:bg-slate-850 px-3 rounded text-slate-700 dark:text-slate-300">
+    <li 
+        :class="[
+            'flex items-center justify-start text-md p-1 px-3 rounded hover:bg-slate-100 dark:hover:bg-slate-850 text-slate-700 dark:text-slate-300',
+            disabled ? 'opacity-25 cursor-not-allowed' : 'opacity-100 cursor-pointer'
+        ]"
+        :aria-disabled="disabled"
+    >
         <slot></slot>
     </li>
 </template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+
+export default defineComponent({
+    props: {
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    setup(props) {
+        const disabled = computed(() => props.disabled)
+
+        return {
+            disabled
+        }
+    }
+})
+</script>
