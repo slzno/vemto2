dae84d392274bea4d6f0394ba9c4a9e766c5e3d8
Tiago S. P. Rodrigues <tiagosilvapereira3@gmail.com>
2025-09-23 15:08:04 -0300

    Adding more Model tests

--- src/common/models/Model.test.ts
+++ src/common/models/Model.test.ts
@@ -147,3 +147,113 @@ test('It can render the template with an invalid relationship', async () => {
 
     expect(contentIsEqual).toBe(true)
 })
+
+test('It validates model correctly', async () => {
+    const userModel = TestHelper.createModel()
+
+    expect(userModel.isValid()).toBe(true)
+    expect(userModel.isInvalid()).toBe(false)
+    expect(userModel.hasTable()).toBe(true)
+    expect(userModel.hasNoTable()).toBe(false)
+})
+
+test('It invalidates model without name', async () => {
+    const userModel = TestHelper.createModel()
+
+    userModel.name = ''
+    userModel.save()
+
+    expect(userModel.isValid()).toBe(false)
+    expect(userModel.isInvalid()).toBe(true)
+})
+
+test('It invalidates removed model', async () => {
+    const userModel = TestHelper.createModel()
+
+    userModel.removed = true
+    userModel.save()
+
+    expect(userModel.isValid()).toBe(false)
+    expect(userModel.isInvalid()).toBe(true)
+})
+
+test('It checks if model has parent class', async () => {
+    const userModel = TestHelper.createModel()
+
+    expect(userModel.hasParentClass()).toBe(false) // Not set by default in createModel
+
+    userModel.parentClass = "Illuminate\\Database\\Eloquent\\Model"
+    userModel.save()
+
+    expect(userModel.hasParentClass()).toBe(true)
+    expect(userModel.getParentClass()).toBe("Illuminate\\Database\\Eloquent\\Model")
+})
+
+test('It checks if model has traits', async () => {
+    const userModel = TestHelper.createModel()
+
+    expect(userModel.hasTraits()).toBe(false) // Not set by default in createModel
+
+    userModel.traits = ["Illuminate\\Database\\Eloquent\\Factories\\HasFactory"]
+    userModel.save()
+
+    expect(userModel.hasTraits()).toBe(true)
+    expect(userModel.getTraits()).toContain("Illuminate\\Database\\Eloquent\\Factories\\HasFactory")
+})
+
+test('It checks if model has interfaces', async () => {
+    const userModel = TestHelper.createModel()
+
+    expect(userModel.hasInterfaces()).toBe(false)
+
+    userModel.interfaces = ['UserInterface']
+    userModel.save()
+
+    expect(userModel.hasInterfaces()).toBe(true)
+    expect(userModel.getInterfaces()).toContain('UserInterface')
+})
+
+test('It checks if model has methods', async () => {
+    const userModel = TestHelper.createModel()
+
+    expect(userModel.hasMethods()).toBe(false)
+
+    userModel.methods = ['getFullName']
+    userModel.save()
+
+    expect(userModel.hasMethods()).toBe(true)
+    expect(userModel.getMethods()).toContain('getFullName')
+})
+
+test('It returns controller name', async () => {
+    const userModel = TestHelper.createModel({ name: 'User' })
+
+    expect(userModel.getControllerName()).toBe('UserController')
+})
+
+test('It returns seeder quantity', async () => {
+    const userModel = TestHelper.createModel()
+
+    expect(userModel.getSeederQuantity()).toBe(5) // Default value
+})
+
+test('It returns primary key name', async () => {
+    const userModel = TestHelper.createModel()
+
+    expect(userModel.getPrimaryKeyName()).toBe('id') // Default primary key
+})
+
+test('It checks if primary key is unusual', async () => {
+    const userModel = TestHelper.createModel()
+
+    expect(userModel.hasUnusualPrimaryKeyName()).toBe(false)
+})
+
+test('It returns class string', async () => {
+    const userModel = TestHelper.createModel({ name: 'User' })
+
+    userModel.namespace = 'App\\Models'
+    userModel.save()
+
+    expect(userModel.getClassString()).toBe('App\\Models\\User')
+})
